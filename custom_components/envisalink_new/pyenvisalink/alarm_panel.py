6be0292e4acbe5aca9d7093b0d5a5f03958174ca
disforw <github@benzona.net>
2023-06-19 21:18:13 -0400

    Update alarm_panel.py
    Signed-off-by: @disforw <github@benzona.net>

--- custom_components/envisalink_new/pyenvisalink/alarm_panel.py
+++ custom_components/envisalink_new/pyenvisalink/alarm_panel.py
@@ -361,7 +361,7 @@ class EnvisalinkAlarmPanel:
         else:
             _LOGGER.error(COMMAND_ERR)
 
-    async def toggle_chime (self, code)
+    async def toggle_chime (self, code):
         """Public method to toggle chime."""
         if self._client:
             await self._client.toggle_chime(code)
