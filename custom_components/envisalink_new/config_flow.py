580528568eda122e6eb65dd75ac34c572183a332
David O'Neill <ufodone@gmail.com>
2024-12-24 19:52:44 -0800

    Fixed deprecation warning (#162)

--- custom_components/envisalink_new/config_flow.py
+++ custom_components/envisalink_new/config_flow.py
@@ -109,15 +109,11 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         config_entry: config_entries.ConfigEntry,
     ) -> config_entries.OptionsFlow:
         """Create the options flow."""
-        return OptionsFlowHandler(config_entry)
+        return OptionsFlowHandler()
 
 
 class OptionsFlowHandler(config_entries.OptionsFlow):
     """Handle Envisalink options."""
-
-    def __init__(self, config_entry: config_entries.ConfigEntry) -> None:
-        """Initialize options flow."""
-        self.config_entry = config_entry
 
     async def async_step_init(self, user_input: dict[str, Any] | None = None) -> FlowResult:
         """Manage the options."""
