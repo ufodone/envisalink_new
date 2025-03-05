654c88abcc9c8f047d46455b5c71984d0cfda742
David O'Neill <ufodone@gmail.com>
2025-03-04 21:05:20 -0800

    Config git before commit/push

--- scripts/sync-wiki.sh
+++ scripts/sync-wiki.sh
@@ -3,16 +3,11 @@
 src_dir=$1
 dst_dir=$2
 
-echo "PWD:$(pwd)"
-
-echo "src:"
-ls -la $src_dir
-echo "dst:"
-ls -la $dst_dir
-
 if [ -d $src_dir ]; then
     cp -R $src_dir/* $dst_dir
     cd $dst_dir
+    git config --global user.name "David O'Neill"
+    git config --global user.email "ufodone@gmail.com"
     git add .
     git commit -m "Sync wiki changes"
     git push
