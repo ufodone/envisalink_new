3609858ef8127c223cfd64c560a33d4bd572a28b
David O'Neill <ufodone@gmail.com>
2025-03-04 21:30:03 -0800

    Fixed syntax error

--- scripts/sync-wiki.sh
+++ scripts/sync-wiki.sh
@@ -4,7 +4,7 @@ base_repo_dir=$1
 src_wiki_dir=$base_repo_dir/wiki
 wiki_depo_dir=$2
 
-if [ -d $src_wiki_dir]; then
+if [ -d $src_wiki_dir ]; then
     cp -R $src_wiki_dir/* $wiki_depo_dir
     cd $wiki_depo_dir
     git config --global user.name "$GITHUB_ACTOR"
