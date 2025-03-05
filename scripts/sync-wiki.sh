9211443f94948f6fda50b968d45fb30b916c0b91
David O'Neill <ufodone@gmail.com>
2025-03-04 20:59:48 -0800

    Change wiki workflow to be run manually

--- scripts/sync-wiki.sh
+++ scripts/sync-wiki.sh
@@ -3,9 +3,16 @@
 src_dir=$1
 dst_dir=$2
 
+echo "PWD:$(pwd)"
+
+echo "src:"
+ls -la $src_dir
+echo "dst:"
+ls -la $dst_dir
+
 if [ -d $src_dir ]; then
     cp -R $src_dir/* $dst_dir
-    cd $dir_dir
+    cd $dst_dir
     git add .
     git commit -m "Sync wiki changes"
     git push
