3f8e8c93165c2273466b505e6518bfe0f4fd8f79
David O'Neill <ufodone@gmail.com>
2025-03-04 21:25:10 -0800

    Re-enable wiki publish on push

--- scripts/sync-wiki.sh
+++ scripts/sync-wiki.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-src_dir=$1
-dst_dir=$2
+base_repo_dir=$1
+src_wiki_dir=$base_repo_dir/wiki
+wiki_depo_dir=$2
 
-if [ -d $src_dir ]; then
-    cp -R $src_dir/* $dst_dir
-    cd $dst_dir
-    git config --global user.name "David O'Neill"
-    git config --global user.email "ufodone@gmail.com"
+if [ -d $src_wiki_dir]; then
+    cp -R $src_wiki_dir/* $wiki_depo_dir
+    cd $wiki_depo_dir
+    git config --global user.name "$GITHUB_ACTOR"
+    git config --global user.email "$GITHUB_ACTOR@users.noreply.github.com"
     git add .
     git commit -m "Sync wiki changes"
     git push
